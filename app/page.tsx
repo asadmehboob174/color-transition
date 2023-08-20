24ab8bb693a49f9349b5fa4a6430d25a02aa3420
asad_aixi <asad.mehboob@probaseapps.com>
2023-08-21 04:12:25 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
 import Image from 'next/image'
 
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Hero  from '@/components/Hero'
+import Copyright from '@/components/Copyright'
+import Cars from '@/components/Cars'
+import CarSlider from '@/components/CarSlider'
+import HoursSelection from '@/components/HoursSelection'
+import Search from '@/components/Search'
+import SearchMobile from '@/components/SearchMobile'
+import LocationSelection from '@/components/LocationSelection'
+import Cta  from '@/components/Cta'
+import Brands from '@/components/Brands'
+import BackToTopBtn from '@/components/BackToTopBtn'
+import About  from '@/components/About'
+import Why from '@/components/Why'
+import Testimonials from '@/components/Testimonials'
+import TestimonialSlider from '@/components/TestimonialSlider'
+import DateSelection from '@/components/DateSelection'
+
+
 export default function Home() {
   return (
-    <main>Asad</main>
+    <main className='max-w-[1920px] bg-white
+     mx-auto relative overflow-hidden 
+    '>
+      <Header />
+      {/* <Hero />
+      <Cars />
+      <About />
+      <Why />
+      <Testimonials />
+      <Cta />
+      <Footer />
+      <BackToTopBtn /> */}
+    </main>
   )
 }
